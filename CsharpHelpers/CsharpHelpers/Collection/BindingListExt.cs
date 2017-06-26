10519aae8923862a9a66c261e8721c226fbf6565
Fedor <novikovfedor@gmail.com>
2017-06-26 23:33:09 +0300

    add few fixes

--- CsharpHelpers/CsharpHelpers/Collection/BindingListExt.cs
+++ CsharpHelpers/CsharpHelpers/Collection/BindingListExt.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 
-namespace Helper.Extensions
+namespace CsharpHelpers.Collection
 {
     public class BindingListExt<T> : BindingList<T>
     {
---
+++ CsharpHelpers/CsharpHelpers/Singleton.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpHelpers
+{
+    public static class Singleton<T>
+        where T : new()
+    {
+        private static T _instance;
+        public static T Instance => _instance == null ? (_instance = new T()) : _instance;
+    }
+}
